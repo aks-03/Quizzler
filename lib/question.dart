491b38c1a5291784493a69c9fb1bc1fb9b6a160b
Akshay <gehlotakshay10@gmail.com>
2022-07-21 22:12:52 +0530

    project 4

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+  //Question({required this.questionText, required this.questionAnswer});
+  Question({required String q, required bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
